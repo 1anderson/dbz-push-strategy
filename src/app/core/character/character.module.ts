f3264dd0a5d9e4e0d18eada8618669822054c7e6
anderson sobral <anderson.sobral@ctis.com.br>
2020-08-26 17:30:19 -0300

    esqueleto do projeto

---
+++ src/app/core/character/character.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharacterListComponent } from './components/character-list/character-list.component';
+import { CharacterComponent } from './components/character/character.component';
+
+
+
+@NgModule({
+  declarations: [CharacterListComponent, CharacterComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharacterModule { }
